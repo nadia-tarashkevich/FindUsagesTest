845ff854aa47b36c566e4e592b8c5e16a9ee6268
nadia.tarashkevich <nadia.tarashkevich@jetbrains.com>
2022-05-18 09:50:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.jetbrains.support.externallib.ExternalLib;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ExternalLib l = new ExternalLib();
+        l.SomeFunction();
+    }
+}
